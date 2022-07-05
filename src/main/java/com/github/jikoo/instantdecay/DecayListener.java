8c8eedc556cea7ec23f951c8a8d2c5d484a73013
Jikoo <jikoo.games@gmail.com>
2022-07-05 17:33:00 -0400

    Widen radius, clean up chunk load prevention
    Theoretically it's impossible to hit the chunk load prevention anyway, but this way unloaded chunks do not prevent decay entirely.
    Leaves now always spawn particles, but still only play extra sounds 1/10 of the time.

--- src/main/java/com/github/jikoo/instantdecay/DecayListener.java
+++ src/main/java/com/github/jikoo/instantdecay/DecayListener.java
@@ -1,13 +1,16 @@
 package com.github.jikoo.instantdecay;
 
-import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.SoundCategory;
 import org.bukkit.Tag;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Leaves;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
@@ -18,7 +21,6 @@ import java.util.Random;
 
 public class DecayListener implements Listener {
 
-
 	private final Random random = new Random();
 	private final @NotNull Plugin plugin;
 
@@ -26,86 +28,96 @@ public class DecayListener implements Listener {
 		this.plugin = plugin;
 	}
 
-	@EventHandler
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
 	public void blockBreakEvent(@NotNull BlockBreakEvent event) {
-
-		if (!Tag.LOGS.isTagged(event.getBlock().getType()) && !Tag.LEAVES.isTagged(event.getBlock().getType())) {
-			return;
-		}
-
 		Block block = event.getBlock();
-		final World world = block.getWorld();
-		final int x = block.getX();
-		final int y = block.getY();
-		final int z = block.getZ();
-		final int range = 4;
-		final int off = range + 1;
-
-		if (!allChunksLoaded(world, x - off, y - off, z - off, x + off, y + off, z + off)) {
+		Material broken = block.getType();
+		if (!Tag.LOGS.isTagged(broken) && !Tag.LEAVES.isTagged(broken)) {
 			return;
 		}
 
-		plugin.getServer().getScheduler().runTaskLater(
-				plugin,
-				() -> decayLeavesAround(world, x, y, z, range),
-				1L);
+		plugin.getServer().getScheduler().runTaskLater(plugin, () -> decayLeavesAround(block), 1L);
 	}
 
-	private void decayLeavesAround(@NotNull World world, int x, int y, int z, int range) {
-		for (int offX = -range; offX <= range; offX++) {
-			for (int offZ = -range; offZ <= range; offZ++) {
-				for (int offY = -range; offY <= range; offY++) {
-					Block blockLeaves = world.getBlockAt(x + offX, y + offY, z + offZ);
-					BlockData data = blockLeaves.getBlockData();
-					if (!(data instanceof Leaves leaves)) {
+	private void decayLeavesAround(@NotNull Block block) {
+		World world = block.getWorld();
+
+		int range = 8;
+
+		int minX = block.getX() - range;
+		int maxX = block.getX() + range;
+		int minZ = block.getZ() - range;
+		int maxZ = block.getZ() + range;
+		// Clamp Y to world bounds.
+		int minY = Math.max(block.getY() - range, world.getMinHeight());
+		int maxY = Math.min(block.getY() + range, world.getMaxHeight());
+
+		int lastChunkX = minX >> 4;
+		int highestChunkZ = minZ >> 4 - 1;
+
+		for (int x = minX; x <= maxX; ++x) {
+			int currentChunkX = x >> 4;
+			// If the X change results in interaction with new chunks, reset max chunk Z.
+			if (currentChunkX != lastChunkX) {
+				lastChunkX = currentChunkX;
+				highestChunkZ = minZ >> 4 - 1;
+			}
+
+			for (int z = minZ; z <= maxZ; ++z) {
+				int currentChunkZ = z >> 4;
+				// If the new chunk Z exceeds the old, this is a new chunk. Check that it is loaded.
+				if (currentChunkZ > highestChunkZ) {
+					highestChunkZ = currentChunkZ;
+
+					// If chunk is unloaded, skip to next chunk.
+					if (!world.isChunkLoaded(currentChunkX, currentChunkZ)) {
+						// We add 15 instead of 16 for a full chunk because the loop will add another 1.
+						// We convert back from current chunk to block coordinates in case minZ is not on the chunk border.
+						z = currentChunkZ << 4 + 15;
 						continue;
-					}
-					if (leaves.isPersistent() || leaves.getDistance() < 7) {
-						continue;
-					}
-					LeavesDecayEvent event = new LeavesDecayEvent(blockLeaves);
-					plugin.getServer().getPluginManager().callEvent(event);
-
-					if (event.isCancelled()) {
-						return;
-					}
-
-					blockLeaves.breakNaturally();
-
-					if (random.nextInt(10) == 0) {
-						world.playEffect(blockLeaves.getLocation(), Effect.STEP_SOUND, Material.OAK_LEAVES);
 					}
 				}
-			}
-		}
-	}
 
-	private boolean allChunksLoaded(
-			@NotNull World world,
-			int minX,
-			int minY,
-			int minZ,
-			int maxX,
-			int maxY,
-			int maxZ) {
-		if (maxY < world.getMinHeight() || minY > world.getMaxHeight()) {
-			return false;
-		}
-
-		minX >>= 4;
-		minZ >>= 4;
-		maxX >>= 4;
-		maxZ >>= 4;
-
-		for (int x = minX; x <= maxX; x++) {
-			for (int z = minZ; z <= maxZ; z++) {
-				if (!world.isChunkLoaded(x, z)) {
-					return false;
+				for (int y = minY; y <= maxY; ++y) {
+					// Handle decay for each block.
+					decayLeaves(world.getBlockAt(x, y, z));
 				}
 			}
 		}
 
-		return true;
+	}
+
+	private void decayLeaves(@NotNull Block block) {
+		Material material = block.getType();
+		BlockData data = block.getBlockData();
+
+		if (!(data instanceof Leaves leaves) || isDecayPrevented(block, leaves) || !block.breakNaturally()) {
+			return;
+		}
+
+		Location location = block.getLocation();
+		block.getWorld().spawnParticle(Particle.BLOCK_CRACK, location, 1, material);
+
+		if (random.nextInt(10) == 0) {
+			block.getWorld().playSound(location, getSound(material), SoundCategory.BLOCKS, 1F, 1F);
+		}
+	}
+
+	private boolean isDecayPrevented(@NotNull Block block, @NotNull Leaves leaves) {
+		if (leaves.isPersistent() || leaves.getDistance() <= 7) {
+			return true;
+		}
+
+		LeavesDecayEvent event = new LeavesDecayEvent(block);
+		plugin.getServer().getPluginManager().callEvent(event);
+
+		return event.isCancelled();
+	}
+
+	private @NotNull String getSound(@NotNull Material material) {
+		return material == Material.AZALEA_LEAVES || material == Material.FLOWERING_AZALEA_LEAVES
+				? "block.azalea_leaves.break"
+				: "block.grass.break";
 	}
 
 }
